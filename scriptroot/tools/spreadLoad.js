786f736ca97949504429b2cdc40b633432ce774c
tokugero <palmueti@gmail.com>
2022-01-09 23:08:07 -0800

    hardening spreadLoad to better accomodate local node hacking

--- scriptroot/tools/spreadLoad.js
+++ scriptroot/tools/spreadLoad.js
@@ -1,17 +1,24 @@
+import * as mapServers from '/tools/mapServers.js';
+
 /** @param {import("../../common/.").NS} ns */
-import * as mapServers from 'mapServers.js';
 
 export async function main(ns){
     const args = ns.flags([["help", false]]);
 
-    var allServers = mapServers.getAll();
-
     var freeSpace = 14;
     var localhost = ns.getServer();
-    var threads = (localhost.maxRam-freeSpace)/Math.ceil(ns.getScriptRam("/hacks/hgw.js", localhost.hostname));
-    var endpoint = args._[0];
-    while(true){
-        ns.exec('hacks/hgw.js', localhost.hostname, threads, endpoint);
+    var threads = (localhost.maxRam-freeSpace)/Math.ceil(ns.getScriptRam("/tools/spreadLoad.js", localhost.hostname));
+
+    var allServers = await mapServers.getAllServers(ns);
+    while (true) {
+        var worstServer = ns.getServer("joesguns");
+        for (const server of allServers) {
+
+            if ( server.hasAdminRights && worstServer.serverGrowth < server.serverGrowth) {
+                worstServer = server;
+            };
+        };
+        ns.exec("/hacks/hgw.js", localhost.hostname, threads, worstServer.hostname);
         await ns.sleep(100);
-    };
+	};
 }
