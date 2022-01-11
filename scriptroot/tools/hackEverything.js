933dd6c52126688ee21379b98f8dcb05d84eb987
tokugero <palmueti@gmail.com>
2022-01-11 12:05:49 -0800

    Fixing bug with port counts not being sufficient to start nuking server.

--- scriptroot/tools/hackEverything.js
+++ scriptroot/tools/hackEverything.js
@@ -15,15 +15,17 @@ export async function gracefulHack(ns){
     var mylevel = ns.getHackingLevel();
 
     for (const server of allServers){
-        if (mylevel >= ns.getServerRequiredHackingLevel(server.hostname)) {
-            if (!server.sshPortOpen && ns.fileExists("BruteSSH.exe", "home")) { ns.brutessh(server.hostname); };
-            if (!server.ftpPortOpen && ns.fileExists("FTPCrack.exe", "home")) { ns.ftpcrack(server.hostname); };
-            if (!server.sqlPortOpen && ns.fileExists("SQLInject.exe", "home")) { ns.sqlinject(server.hostname); };
-            if (!server.httpPortOpen && ns.fileExists("HTTPWORM.exe", "home")) { ns.httpworm(server.hostname); };
-            if (!server.smtpPortOpen && ns.fileExists("relaySMTP.exe", "home")) { ns.relaysmtp(server.hostname); };
+        var isRoot = server.hasAdminRights;
+        if (mylevel >= ns.getServerRequiredHackingLevel(server.hostname) && !isRoot) {
 
-            var isRoot = server.hasAdminRights;
-            while (!isRoot && server.openPortCount > server.numOpenPortsRequired){
+            if (!server.sshPortOpen && ns.fileExists("BruteSSH.exe", "home")) { ns.toast(`Bruteforcing ${server.hostname}: ${ns.brutessh(server.hostname)}`); };
+            if (!server.ftpPortOpen && ns.fileExists("FTPCrack.exe", "home")) { ns.toast(`FTPCracking ${server.hostname}: ${ns.ftpcrack(server.hostname)}`) };
+            if (!server.sqlPortOpen && ns.fileExists("SQLInject.exe", "home")) { ns.toast(`SQLInjecting ${server.hostname}: ${ns.sqlinject(server.hostname)}`) };
+            if (!server.httpPortOpen && ns.fileExists("HTTPWORM.exe", "home")) { ns.toast(`HTTPWorming ${server.hostname}: ${ns.httpworm(server.hostname)}`) };
+            if (!server.smtpPortOpen && ns.fileExists("relaySMTP.exe", "home")) { ns.toast(`relaySMTPing ${server.hostname}: ${ns.relaysmtp(server.hostname)}`) };
+
+            
+            while (!isRoot && server.openPortCount >= server.numOpenPortsRequired){
                 ns.nuke(server.hostname);
                 await ns.sleep(20);
                 isRoot = ns.hasRootAccess(server.hostname);
@@ -35,7 +37,7 @@ export async function gracefulHack(ns){
                         var threads = manageServer.usableThreads(ns, server, "hacks/hgw.js");
                         ns.exec('hacks/hgw.js', server.hostname, threads, server.hostname);
                     };
-                }
+                };
             };
         };
         await ns.sleep(20);
