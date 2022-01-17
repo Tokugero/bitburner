e5fd8170cb4eec8343aa5650574340a6417afc08
tokugero <palmueti@gmail.com>
2022-01-17 00:05:22 -0800

    Fixing dependency map

--- scriptroot/tools/grafana.js
+++ scriptroot/tools/grafana.js
@@ -11,5 +11,5 @@ Manage statistics that need to regularly be sent to grafana.
 export async function main(ns){
     let playerMoney = ns.getServerMoneyAvailable("home");
     await ns.wget(`${url}/money?secret=${secret}&money=${playerMoney}`, `/dev/null.txt`);
-    ns.spawn("./tools/grafana.js");
+    ns.spawn("tools/grafana.js");
 };
