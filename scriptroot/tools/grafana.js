aaa2bd86d76ffb5bb5b86c1938cb64bac33c5be5
tokugero <palmueti@gmail.com>
2022-01-17 22:10:01 -0800

    Fixing for reference

--- scriptroot/tools/grafana.js
+++ scriptroot/tools/grafana.js
@@ -12,7 +12,7 @@ Manage statistics that need to regularly be sent to grafana.
 export async function main(ns){
     let playerMoney = ns.getServerMoneyAvailable("home");
     await ns.wget(`${url}player=1&money=${playerMoney}`, `/dev/null.txt`);
-    for (const serverDetails in await mapServers.getAllServers(ns)){
+    for (const serverDetails of await mapServers.getAllServers(ns)){
         await ns.wget(`${url}server=${serverDetails.hostname}&maxRam=${serverDetails.maxRam}&usedRam=${serverDetails.ramUsed}&cpu=${serverDetails.cpuCores}&moneyMax=${serverDetails.moneyMax}&moneyAvail=${serverDetails.moneyAvailable}&hacked=${(serverDetails.hasAdminRights ? 1 : 0)}&owned=${(serverDetails.purchasedByPlayer ? 1 : 0)}`, `/dev/null.txt`);
     };
     ns.spawn("tools/grafana.js");
