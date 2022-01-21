f0dd9359f174a0a8b9e67c06dffe41081c92ece1
James Moore <jhagrid77@gmail.com>
2022-01-21 14:15:24 -0500

    Provide info on failed contract attempts? (#4)
    * Provide info on failed contract attempts?
    
    * Update based upon feedback
    
    * Update based upon feedback

--- scriptroot/tools/contractHandler.js
+++ scriptroot/tools/contractHandler.js
@@ -30,8 +30,15 @@ export async function handle(ns, contractFile, server, contractType){
     };
     if (answer !== ""){
         let reward = ns.codingcontract.attempt(answer, contractFile, server.hostname, {returnReward: true});
-        ns.tprint(`Contract ${contractType} solved for ${reward}`);
-        return reward;
+        if (reward !== false){
+            ns.tprint(`Contract ${contractType} solved for ${reward}`);
+            return reward;
+        } else {
+            ns.tprint(`Attempted to solve ${contractType} on ${server.hostname} with the following answer, but it failed:
+            \t${answer}
+            The info was:
+            \t${ns.codingcontract.getData(contractFile, server.hostname)} `);
+        }
     } else {
         return "Just kidding, you failed to answer anything. Debug me!";
     };
